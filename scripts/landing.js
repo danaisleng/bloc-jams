abed5b448d926d9bab17ed3cdfff6f084f88926c
Colin Tracy <colin@aisleng.com>
2017-08-02 14:47:51 -0600

    add utilities with callback function

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,20 @@
-var sellingPoints = document.getElementById('sellingPoints');
-function offset(elt) {
-    var rect = elt.getBoundingClientRect(), bodyElt = document.body;
+var pointsArray = document.getElementsByClassName('point');
 
-    return {
-      top: rect.top + bodyElt .scrollTop,
-      left: rect.left + bodyElt .scrollLeft
-    }
-};
-
-var offset = offset(sellingPoints);
-
-window.onscroll = function() {
-    if (window.pageYOffset > (offset.top - 350)) {
-        revealPoint('point');
-    };
+var animatePoints = function(points) {
+    forEach(pointsArray, executed);
 }
 
-
-
-function revealPoint(points){
-    var points = document.getElementsByClassName(points);
-
-    for(var i = 0; i < points.length; i++){
-        points[i].style.opacity = 1;
-        points[i].style.transform = 'scaleX(1) translateY(0)';
-        points[i].style.msTransform = 'scaleX(1) translateY(0)';
-        points[i].style.WebkitTransform = 'scaleX(1) translateY(0)';
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
     }
-};
+
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event){
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);   
+        }
+    });
+}
---
+++ scripts/utilities.js
@@ -0,0 +1,13 @@
+function forEach(points, callback) {
+	for (var i = 0; i < points.length; i++) {
+		points[i].style.opacity = 1;
+        points[i].style.transform = 'scaleX(1) translateY(0)';
+        points[i].style.msTransform = 'scaleX(1) translateY(0)';
+        points[i].style.WebkitTransform = 'scaleX(1) translateY(0)';          
+        callback(i);
+	};
+}
+
+function executed(index) {
+	console.log('Element ' + index + ' executed properly');
+}
